e84f870b9a1d0fb8ab57aa918a2441b1d1e37d73
Pabluu <pabloph0077@gmail.com>
2022-07-15 12:10:41 -0300

    arquivo js melhorado

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-function getFile(opcao) {
-    let label, miniLabel, arquivo;
+function getFile(pai) {
+    const inputFile = pai.children[1];
+    const miniLabel = pai.children[2];
 
-    // primeiro input
-    if (opcao === 'primary') {
-        label = document.getElementById('pl');
-        miniLabel = document.getElementById('pl-mini');
-        arquivo = document.getElementById('pf');
-    } else {
-        // segundo input
-        label = document.getElementById('sl');
-        miniLabel = document.getElementById('sl-mini');
-        arquivo = document.getElementById('sf');
-    }
-
-    arquivo.addEventListener('change', function () {
+    inputFile.addEventListener('change', function () {
         let nomeArquivo = '';
-        if (arquivo.files.length > 0) {
-            nomeArquivo = arquivo.files[0].name;
+        if (inputFile.files.length > 0) {
+            nomeArquivo = inputFile.files[0].name;
         }
 
-        miniLabel.innerHTML = removeExtensao(nomeArquivo);
+        // remove a extensao do arquivo(replace)
+        miniLabel.innerHTML = nomeArquivo.replace(/\.[^/.]+$/, "");
     })
-}
-
-function removeExtensao(file){
-    let fileWithoutExtension = file.replace(/\.[^/.]+$/, "");
-
-    console.log(fileWithoutExtension);
-    return fileWithoutExtension;
 }
