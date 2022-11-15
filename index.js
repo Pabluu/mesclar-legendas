bb82915741b51cc4fb772b0ae8a1fbbc338b3193
Pablo Henrique <pabloph0077@gmail.com>
2022-11-14 22:57:19 -0300

    lendo os arquivos

--- index.js
+++ index.js
@@ -4,19 +4,54 @@ function getFile(inputFile) {
 
     miniLabel.style.backgroundColor = 'white';
 
+    debugger
     // obtendo nome do arquivo
-    let nomeArquivo = '';
+    let nameFile = '';
     if (inputFile.files.length > 0) {
-        nomeArquivo = inputFile.files[0].name;
+        nameFile = getName(inputFile)
+        miniLabel.innerHTML = getNameWithoutName(nameFile);
     }
-
-    // removendo a extensao do arquivo
-    miniLabel.innerHTML = nomeArquivo.replace(/\.[^/.]+$/, "");
 
     // alterando a cor de fundo
     if (miniLabel.innerHTML !== '') {
         miniLabel.style.backgroundColor = '#080f6140';
     }
 
-    
 }
+
+// nome do arquivo
+function getName(inputFile) {
+    return inputFile.files[0].name;
+}
+
+// nome do arquivo SEM EXTENSÃO
+function getNameWithoutName(nameFile) {
+    return nameFile.replace(/\.[^/.]+$/, "");
+}
+
+let button = document.querySelector('button');
+let inputs = document.querySelectorAll('input');
+let conteudo = []
+
+inputs.forEach(input => {
+    input.addEventListener('change', function () {
+        let file = new FileReader();
+        getFile(input);
+
+        file.onload = () => {
+            conteudo.push(file.result)
+        }
+
+        file.readAsText(this.files[0]);
+    })
+})
+
+
+button.addEventListener('click', function () {
+    if (conteudo != '') {
+        // debugger
+        for (index in conteudo) {
+            console.log(index)
+        }
+    }
+})
